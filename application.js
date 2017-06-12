b23da815af0fba0a5179b9c61038423af0bdd521
torbjorndahlen <torbjorn.dahlen@icloud.com>
2017-06-12 08:58:53 +0200

    form.js

--- application.js
+++ application.js
@@ -17,9 +17,9 @@ app.use('/sys', mbaasExpress.sys(securableEndpoints));
 app.use('/mbaas', mbaasExpress.mbaas);
 
 /* uncomment this code if you want to use $fh.auth in the app preview
- * localAuth is only used for local development. 
- * If the app is deployed on the platform, 
- * this function will be ignored and the request will be forwarded 
+ * localAuth is only used for local development.
+ * If the app is deployed on the platform,
+ * this function will be ignored and the request will be forwarded
  * to the platform to perform authentication.
 
 app.use('/box', mbaasExpress.auth({localAuth: function(req, cb){
@@ -38,6 +38,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(mbaasExpress.fhmiddleware());
 
 app.use('/hello', require('./lib/hello.js')());
+app.use('/form', require('./lib/form.js')());
 
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
@@ -45,5 +46,5 @@ app.use(mbaasExpress.errorHandler());
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.listen(port, host, function() {
-  console.log("App started at: " + new Date() + " on port: " + port); 
+  console.log("App started at: " + new Date() + " on port: " + port);
 });
